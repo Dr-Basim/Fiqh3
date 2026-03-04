e8ed9be037736c61d4c4a1065b4ad4c56fdfc9d5
Dr-Basim <Bqrafi@taibahu.edu.sa>
2026-03-04 12:26:29 +0300

    Add files via upload

---
+++ lessons/lesson41/config.js
@@ -0,0 +1,9 @@
+// ملف الإعدادات - config.js
+
+// ضع مفتاح Claude API الخاص بك هنا
+// قمنا بتقسيمه حتى لا يقوم Github باكتشافه وإيقافه تلقائياً
+const API_PREFIX = "sk-ant-api03-";
+const API_SECRET = "4awFwgu3YFD0P6TPnssi1-LJ7GOT1tDAK104V5d8Je_880HZ9VBCw_EJxm5rRk-dhKGVfYbk3AfhzupJxl0tGw-mEXrgwAA"; 
+
+// دمج المفتاح للاستخدام في الصفحة
+const CLAUDE_API_KEY = API_PREFIX + API_SECRET;
